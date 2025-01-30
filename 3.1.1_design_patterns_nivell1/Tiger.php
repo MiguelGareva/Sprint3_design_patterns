2b5657f187af7ba3e09e294675bf14f2c6a6b993
miguel garcia <miguel.gareva@gmail.com>
2025-01-30 12:49:21 +0100

    Tigger class and his methods created

--- 3.1.1_design_patterns_nivell1/Tiger.php
+++ 3.1.1_design_patterns_nivell1/Tiger.php
@@ -1,4 +1,26 @@
 <?php 
+declare(strict_types=1);
+class Tigger{
 
+    private static Tigger $instance;
+    private int $rugidos = 0;
+
+    private function __construct(){
+        echo "Building character..." . PHP_EOL;
+    }
+    public function roar():void{
+        echo "Grrrr!" . PHP_EOL;
+        $this->rugidos++;
+    }
+    public static function getInstance():?Tigger{
+        if(self::$instance === null){
+            self::$instance = new Tigger;
+        }
+        return self::$instance;
+    }
+    public function getCounter():int{
+        return $this->rugidos;
+    }
+}
 
 ?>
